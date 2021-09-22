23590934858749bd0783b311d74a4a351b6ab964
Alejandro <57674935+McFlyWare@users.noreply.github.com>
2021-09-21 21:56:55 -0500

    python 1

--- pythonp/cliente.py
+++ pythonp/cliente.py
@@ -6,6 +6,16 @@
 
 import zmq
 
+
+oferta = {
+    "titulo": "",
+    "codigo": "",
+    "sector": "",
+    "experiencia": "",
+    "edad": "",
+    "formacion_academica": ""
+}
+
 context = zmq.Context()
 
 #  Socket to talk to server
@@ -13,11 +23,20 @@ print("Connecting to hello world server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://25.90.3.122:5555")
 
+oferta.update({"titulo": input("ingrese el titulo de la oferta: ")})
+oferta.update({"codigo": input("ingrese el codigo de la oferta: ")})
+oferta.update({"sector": input("ingrese el sector de la oferta: ")})
+oferta.update({"experiencia": input(
+    "ingrese la experiencia del postulante: ")})
+oferta.update({"edad": input("ingrese la edad del postulante: ")})
+oferta.update({"formacion_academica": input(
+    "ingrese la formacion del postulante: ")})
+socket.send_pyobj(oferta)
 #  Do 10 requests, waiting each time for a response
-for request in range(10):
-    print("Sending request %s …" % request)
-    socket.send(b"Hello")
+# for request in range(10):
+#print("Sending request %s …" % request)
+# socket.send(b"Hello")
 
-    #  Get the reply.
-    message = socket.recv()
-    print("Received reply %s [ %s ]" % (request, message))
+# Get the reply.
+#message = socket.recv()
+#print("Received reply %s [ %s ]" % (request, message))
--- pythonp/server.py
+++ pythonp/server.py
@@ -7,14 +7,35 @@
 import time
 import zmq
 
+
+class Oferta:
+    def __init__(self, titulo, codigo, sector, experiencia, edad, formacion_academica):
+        titulo
+        codigo
+        sector
+        experiencia
+        edad
+        formacion_academica
+
+    def obtener_titulo(self):
+        return self.titulo
+
+
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
 
 while True:
     #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+    message = Oferta("", "", "", "", "", "")
+    message = (socket.recv_pyobj())
+    print("llego una nueva oferta laboral")
+    print(message["titulo"])
+    print(message["codigo"])
+    print(message["sector"])
+    print(message["experiencia"])
+    print(message["edad"])
+    print(message["formacion_academica"])
 
     #  Do some 'work'
     time.sleep(1)
