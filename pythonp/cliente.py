334b2283d252c3769e34433d74cd3f1364b6cedf
Alejandro <57674935+McFlyWare@users.noreply.github.com>
2021-09-22 08:47:52 -0500

    maven

--- pythonp/cliente.py
+++ pythonp/cliente.py
@@ -6,7 +6,6 @@
 
 import zmq
 
-
 oferta = {
     "titulo": "",
     "codigo": "",
@@ -16,27 +15,66 @@ oferta = {
     "formacion_academica": ""
 }
 
-context = zmq.Context()
 
+def crear_oferta():
+    oferta.update({"titulo": input("ingrese el titulo de la oferta: ")})
+    oferta.update({"codigo": input("ingrese el codigo de la oferta: ")})
+    oferta.update({"sector": input("ingrese el sector de la oferta: ")})
+    oferta.update({"experiencia": input(
+        "ingrese la experiencia del postulante: ")})
+    oferta.update({"edad": input("ingrese la edad del postulante: ")})
+    oferta.update({"formacion_academica": input(
+        "ingrese la formacion del postulante: ")})
+    socket.send_pyobj(oferta)
+    respuesta = socket.recv_string()
+    print(respuesta)
+
+
+def impresion_menu():
+    print("Menu")
+    print("1). Ingresar como Empleador")
+    print("2). Ingresar como Aspirante")
+    print("3). Salir")
+    i = int(input())
+    print(i)
+    if i == 3:
+        return -1
+    return i
+
+
+def impresion_menu_empleador():
+    print("Menu Empleador")
+    print("1). Crear oferta de empleo")
+    print("2). Salir")
+    i = int(input())
+    if i == 2:
+        return -1
+    return i
+
+
+context = zmq.Context()
 #  Socket to talk to server
-print("Connecting to hello world server…")
+print("--> Iniciando el sistema")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://25.90.3.122:5555")
+print("--> conexion con el servidor establecida correctamente")
+i = 0
+j = 0
+while i != -1:
+    i = impresion_menu()
+    if i == 1:
+        while j != -1:
+            j = impresion_menu_empleador()
+            if j == 1:
+                crear_oferta()
+            else:
+                exit()
 
-oferta.update({"titulo": input("ingrese el titulo de la oferta: ")})
-oferta.update({"codigo": input("ingrese el codigo de la oferta: ")})
-oferta.update({"sector": input("ingrese el sector de la oferta: ")})
-oferta.update({"experiencia": input(
-    "ingrese la experiencia del postulante: ")})
-oferta.update({"edad": input("ingrese la edad del postulante: ")})
-oferta.update({"formacion_academica": input(
-    "ingrese la formacion del postulante: ")})
-socket.send_pyobj(oferta)
 #  Do 10 requests, waiting each time for a response
 # for request in range(10):
-#print("Sending request %s …" % request)
+# print("Sending request %s …" % request)
 # socket.send(b"Hello")
 
 # Get the reply.
-#message = socket.recv()
-#print("Received reply %s [ %s ]" % (request, message))
+# message = socket.recv()
+# print("Received reply %s [ %s ]" % (request, message))
--- pythonp/server.py
+++ pythonp/server.py
@@ -41,4 +41,4 @@ while True:
     time.sleep(1)
 
     #  Send reply back to client
-    socket.send(b"World")
+    socket.send_string("Satisfactorio")
