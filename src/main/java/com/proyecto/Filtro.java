e380863ab6efe2cdcc4f5cd6ad3f2712bc1d79ef
Alejandro <57674935+McFlyWare@users.noreply.github.com>
2021-09-23 20:06:06 -0500

    jueves 8 de la noche

--- src/main/java/com/proyecto/Filtro.java
+++ src/main/java/com/proyecto/Filtro.java
@@ -13,35 +13,105 @@ public class Filtro {
 
       // inicializacion del filtro
       Scanner sc = new Scanner(System.in);
-      System.out.println("Ingrese la direccion ip que se le asignara a el Filtro: ");
-      String dir = sc.nextLine();
-      ZMQ.Socket socketIngreso = context.createSocket(SocketType.REP);
-      ZMQ.Socket socketSalida = context.createSocket(SocketType.REQ);
-      socketIngreso.bind("tcp://*:5555");
-      socketSalida.connect("tcp://"+dir+":3333");
-      System.out.println("--> Filtro iniciado correctamente direccion ip: " + dir + ":5555");
+      ZMQ.Socket socketCliente = context.createSocket(SocketType.REP);
+      ZMQ.Socket socketServidor = context.createSocket(SocketType.REQ);
+      // el Filtro se aloja en el puerto 
+      socketCliente.bind("tcp://*:2222");
+      // el servidor de aloja en los puertos :3333
+      socketServidor.connect("tcp://25.90.9.233:3333");
+      socketServidor2.connect("tcp://25.90.3.122:3333");
+      System.out.println("--> Filtro iniciado correctamente");
 
       // el filtro emieza aescuchar las peticiones
       while (!Thread.currentThread().isInterrupted()) {
+        byte [] respuestasServidor;
+        Integer tamano1, tamano2, tamano3;
         // manejo de la solicitud del cliente
-        byte[] cliente = socketIngreso.recv();
-        socketSalida.send(cliente);
+        byte[] peticionCliente = socketCliente.recv();
+        String peticion = "numeroSolicitudes";
 
-        // manejo de los datos con el servidor
-        byte[] servidor = socketSalida.recv();
-        socketIngreso.send(servidor);
+        // peticion de ofertas alojadas en el servidor 1
+        socketServidor.send(serialize(peticion));
+        // respuesta del servidor con el numero de ofertas guardadas
+        respuestasServidor = socketServidor.recv();
+        tamano1 = -1;
+        if(deserialize(servidor) instanceof Integer)
+        {
+          tamano1 = (Integer)deserialize(servidor); 
+          System.out.println("el servidor tiene en total " + String.valueOf(tamano1) + " ofertas");
+        }
+
+        // peticion de ofertas alojadas en el servidor 2 
+        socketServidor2.send(serialize(peticion));
+        // respuesta del servidor con el numero de ofertas guardadas
+        respuestasServidor = socketServidor2.recv();
+        tamano2 = -1;
+        if(deserialize(servidor) instanceof Integer)
+        {
+          tamano1 = (Integer)deserialize(servidor); 
+          System.out.println("el servidor tiene en total " + String.valueOf(tamano1) + " ofertas");
+        }
+
+        if(tamano1 == Math.min(Math.min(tamano1,tamano2 ),tamano3))
+        {
+          socketServidor.send(peticionCliente);
+
+          // manejo de los datos con el servidor
+          byte[] servidor = socketServidor.recv();
+          socketCliente.send(servidor);
+        }
+        if(tamano2 == Math.min(Math.min(tamano1,tamano2 ),tamano3))
+        {
+          socketServidor2.send(peticionCliente);
+
+          // manejo de los datos con el servidor
+          byte[] servidor = socketServidor2.recv();
+          socketCliente.send(servidor);
+        }
+
+/*
+        // peticion de ofertas alojadas en el servidor3 
+        socketServidor.send(serialize(peticion));
+        // respuesta del servidor con el numero de ofertas guardadas
+        byte[] servidor = socketServidor.recv();
+        tamano3 = -1;
+        if(deserialize(servidor) instanceof Integer)
+        {
+          tamano1 = (Integer)deserialize(servidor); 
+          System.out.println("el servidor tiene en total " + String.valueOf(tamano1) + " ofertas");
+        }
+
+*/
+        String respuesta = "test";
+        socketCliente.send(respuesta);
+
 
         /*
-         * System.out.println( "--> insercion_oferta--> " + "{ " +
-         * ofertaRecivida.getTitulo() + " } " + " { " + ofertaRecivida.getSector() +
-         * " } " + " { " + ofertaRecivida.getCodigo() + " } " + " { " +
-         * ofertaRecivida.getEdad() + " } " + " { " + ofertaRecivida.getExperiencia() +
-         * " } " + " { " + ofertaRecivida.getExperiencia() + " } "); String respuesta =
-         * "satisfactorio"; socket.send(respuesta.getBytes(), 0);
-         */
+        socketServidor.send(peticionCliente);
+
+        // manejo de los datos con el servidor
+        byte[] servidor = socketServidor.recv();
+        socketCliente.send(servidor);
+        */
 
         Thread.sleep(1000);
       }
     }
   }
+    public static byte[] serialize(Object obj) throws IOException {
+        try (ByteArrayOutputStream b = new ByteArrayOutputStream()) {
+            try (ObjectOutputStream o = new ObjectOutputStream(b)) {
+                o.writeObject(obj);
+            }
+            return b.toByteArray();
+        }
+    }
+
+    public static Object deserialize(byte[] bytes) throws IOException, ClassNotFoundException {
+        try (ByteArrayInputStream b = new ByteArrayInputStream(bytes)) {
+            try (ObjectInputStream o = new ObjectInputStream(b)) {
+                return o.readObject();
+            }
+        }
+    }
 }
--- src/main/java/com/proyecto/Servidor.java
+++ src/main/java/com/proyecto/Servidor.java
@@ -18,15 +18,46 @@ public class Servidor {
             Vector<Hashtable> vec_hash = new Vector<>();
             // inicializacion del filtro
             Scanner sc = new Scanner(System.in);
-            System.out.println("Ingrese la direccion ip que se le asignara al Servidor: ");
-            String dir = sc.nextLine();
             ZMQ.Socket socket = context.createSocket(SocketType.REP);
-            // socket.bind("tcp://" + dir + ":5555");
-            socket.bind("tcp://"+dir+":2222");
-            System.out.println("--> Servidor iniciado correctamente direccion ip: " + dir + ":5555");
+            // el filtro se aloja en los puertos :2222
+            socket.bind("tcp://*:3333");
+            System.out.println("--> Servidor iniciado correctamente");
 
             // el Servidor emieza aescuchar las peticiones
             while (!Thread.currentThread().isInterrupted()) {
+                byte[] peticion = socket.recv();
+                if(deserialize(peticion) instanceof  String){
+                    String aux = (String)deserialize(peticion);
+                    System.out.println(aux);
+                    System.out.print("llego una solicitud de peticion de datos");
+                    if(aux.equals("numeroSolicitudes"))
+                    {
+                        System.out.println("entro al if");
+                        Integer tamano = vec_hash.size();
+                        socket.send(serialize(tamano));
+                    }
+                }
+                else if(deserialize(peticion) instanceof  Oferta){
+                    System.out.println("llego una nueva oferta");
+                    Oferta ofertaRecivida = (Oferta) deserialize(peticion);
+
+                    hash_table = new Hashtable<String, String>();
+                    hash_table.put("Titulo", ofertaRecivida.getTitulo());
+                    hash_table.put("Sector", ofertaRecivida.getSector());
+                    hash_table.put("Codigo", String.valueOf(ofertaRecivida.getCodigo()));
+                    hash_table.put("Experiencia", String.valueOf(ofertaRecivida.getExperiencia()));
+                    hash_table.put("Edad", String.valueOf(ofertaRecivida.getEdad()));
+                    hash_table.put("Formacion", ofertaRecivida.getFormacion_academica());
+                    vec_hash.add(hash_table);
+
+                    String respuesta = "satisfactorio";
+                    socket.send(respuesta.getBytes(), 0);
+                    System.out.println(vec_hash);
+
+                }
+ 
+
+/*
                 byte[] reply = socket.recv();
                 Oferta ofertaRecivida = (Oferta) deserialize(reply);
 
@@ -38,19 +69,13 @@ public class Servidor {
                 hash_table.put("Edad", String.valueOf(ofertaRecivida.getEdad()));
                 hash_table.put("Formacion", ofertaRecivida.getFormacion_academica());
                 vec_hash.add(hash_table);
-                /*
-                 * System.out.println( "--> insercion_oferta--> " + "{ " +
-                 * ofertaRecivida.getTitulo() + " } " + " { " + ofertaRecivida.getSector() +
-                 * " } " + " { " + ofertaRecivida.getCodigo() + " } " + " { " +
-                 * ofertaRecivida.getEdad() + " } " + " { " + ofertaRecivida.getExperiencia() +
-                 * " } " + " { " + ofertaRecivida.getExperiencia() + " } "); String respuesta =
-                 * "satisfactorio"; socket.send(respuesta.getBytes(), 0);
-                 */
+
                 String respuesta = "satisfactorio";
                 socket.send(respuesta.getBytes(), 0);
 
                 System.out.println(vec_hash);
                 Thread.sleep(1000);
+                */
             }
         }
     }
